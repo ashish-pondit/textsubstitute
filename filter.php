c0acf29f917fb7c13be97271501de51cce08922e
Ashish Pondit <ashish.pondit@dsinnovators.com>
2024-04-04 16:29:17 +0600

    Add settings for textsubstitute plugin
    Add license
    Modify README.md file

---
+++ filter.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     filter_textsubstitute
+ * @category    string
+ * @copyright   2024 Ashish Pondit <ashish.pondit@dsinnovators.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class filter_textsubstitute extends moodle_text_filter {
+    function filter($text, array $options = []) {
+        if (!isset($options['originalformat'])) {
+            // if the format is not specified, we are probably called by {@see format_string()}
+            // in that case, it would be dangerous to replace text with the image because it could
+            // be stripped. therefore, we do nothing
+            return $text;
+        }
+        if (in_array($options['originalformat'], explode(',', get_config('filter_textsubstitute', 'formats')))) {
+            return $this->replace_word($text);
+        }
+        return $text;
+
+
+
+    }
+
+    protected function replace_word($text) {
+        $searchterm = get_config('filter_textsubstitute', 'searchterm');
+        $replacewith = get_config('filter_textsubstitute', 'substituteterm');
+        $text = str_replace($searchterm, $replacewith, $text);
+        return $text;
+    }
+}
